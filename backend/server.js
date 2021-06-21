1b89c85aae7cbad767e5db9ddc3cfbba079b99d2
junaid.q <junaid.q@asinlabs.tech>
2021-06-22 00:47:51 +0500

    Publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -23,11 +23,15 @@ app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   res.send('Server is ready');
-});
+});*/
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use(express.use(path.join(__dirname,'/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
